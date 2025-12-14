752dcddb3c356e1a3b835a4bbbdf0ffca4045ef7
Dusan Bako <dusan.bako@backbone.sk>
2025-12-14 19:45:33 +0100

    feat: add db models and migrations

---
+++ api/models/__init__.py
@@ -0,0 +1,4 @@
+from api.models.country import Country
+from api.models.organization import Organization
+from api.models.skill import Skill
+from api.models.job import Job
---
+++ api/models/job.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.postgres.search import SearchVectorField
+from django.contrib.postgres.indexes import GinIndex
+
+
+class Job(models.Model):
+    id = models.CharField(max_length=255, primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    organization = models.ForeignKey(
+        'Organization',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    country = models.ForeignKey(
+        'Country',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    skills = models.ManyToManyField('Skill', blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    search_vector = SearchVectorField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['-id']
+        indexes = [
+            GinIndex(fields=['search_vector']),
+        ]
+
+    def __str__(self):
+        return self.title
